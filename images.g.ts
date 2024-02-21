ffc0931f293c387c3cc02c675c54dd26938b966f
felixpablo721 <156235604+felixpablo721@users.noreply.github.com>
2024-02-21 13:01:39 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "chell":return img`
+....................
+.ff.......ff........
+.f1f.....f1f........
+..f1fffff1f.........
+...f11111f..........
+...f1f1f1f..........
+...f11111f..........
+...f111111f.........
+ffff1ffff1ffffffff.f
+f..f11fff1f..f111ff.
+f..f111111f..ff11ff.
+f..f111111f...ffff.f
+...f1ffff1f.........
+...f1f..f1f.........
+...f1ff.f1ff........
+...f111ff111f.......
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
